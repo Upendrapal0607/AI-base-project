7690e49ed05e7f8bf34b902b6b1bff8e2973655b
itsmearun1 <arundevvb1@gmail.com>
2023-10-08 21:38:56 +0530

    images-added-text

--- Front-End/my-open-ai/src/Componants/features/Features.tsx
+++ Front-End/my-open-ai/src/Componants/features/Features.tsx
@@ -24,19 +24,14 @@ export const featuresData = [
 ];
 
 export const Features: React.FC = () => (
-  <div className="w-full flex flex-col md:flex-col">
-    {/* Header Compartment */}
-    <div className="w-full md:flex-1">
-      <div className="md:flex flex-col justify-start items-start text-left mr-20">
-        <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-        Embrace the Future Today and Transform Your Career. It's Time to Make It Happen.
-
-        </h1>
-      </div>
-    </div>
+  <div className="w-full flex flex-col items-center mx-auto bg-gray-100 p-6">
+    {/* Header Text */}
+    <h1 className="z-10 text-4xl font-extrabold text-darkblue bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text">
+      Embrace the Future Today and Transform Your Career. It's Time to Make It Happen.
+    </h1>
 
     {/* Feature Items */}
-    <div className="md:flex-1.5 flex justify-start items-center flex-col w-full md:w-70">
+    <div className="md:w-95% flex flex-col md:flex-row">
       {featuresData.map((item, index) => (
         <Feature title={item.title} text={item.text} key={item.title + index} />
       ))}
@@ -45,13 +40,9 @@ export const Features: React.FC = () => (
 );
 
 export const Feature: React.FC<FeatureProps> = ({ title, text }) => (
-  <div className="w-full flex justify-between flex-row m-4">
-    <div className="flex-1 md:flex-30 max-w-180 mr-8">
-      <h1 className="text-xl font-bold leading-6 text-white">{title}</h1>
-      <div className="w-38 h-3 bg-gradient-bar shadow-md mb-1"></div>
-    </div>
-    <div className="flex-2 md:flex-70 max-w-390">
-      <p className="text-lg text-teal-500">{text}</p>
-    </div>
+  <div className="mb-10">
+    <h1 className="text-xl font-semibold text-teal-500 mb-2">{title}</h1>
+    <div className="w-10 h-1 bg-gradient-bar mb-4"></div>
+    <p className="text-lg text-gray-800">{text}</p>
   </div>
 );
